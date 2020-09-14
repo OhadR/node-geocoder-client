564a8879bd089c505cb67ff12439ea1541a0eeec
OhadR <ohadr.developer@gmail.com>
2020-09-14 21:01:35 +0300

    support mapquest + openstreetmap

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
     npm i node-geocoder
     
+## Environment Variables:
     
+
+to allow `debug`:
+    
+    DEBUG=runner,*
+    
+    
+##### MapQuest Open APIs
+https://developer.mapquest.com/documentation/open/
+
+###### API Key
+
+Important: Usage of the Open APIs requires an API key. If you do not already have one, please sign in to the [MapQuest Developer Network](http://developer.mapquest.com/) and visit the [Keys & Reporting](https://developer.mapquest.com/user/me/apps) page.
--- src/runner.ts
+++ src/runner.ts
@@ -2,7 +2,7 @@ var debug = require('debug')('runner');
 import * as NodeGeocoder from 'node-geocoder';
 
 const options = {
-  provider: 'openmapquest',
+  provider: 'openstreetmap',
   apiKey: 'ddd',
 
   // Optional depending on the providers
@@ -15,7 +15,15 @@ const geocoder = NodeGeocoder(options);
 
 class Main {
 
-  async main()  {
+  async openStreetsMap()  {
+    debug('openStreetsMap()');
+
+    const options = {
+      provider: 'openstreetmap',
+    };
+
+    const geocoder = NodeGeocoder(options);
+
     try {
       const  resultStr = await geocoder.reverse({
         lat: 32.101786566878445,
@@ -34,9 +42,38 @@ class Main {
     }
   }
 
+  async openMapquest ()  {
+    debug('openMapquest()');
 
+    const options = {
+      provider: 'openmapquest',
+      apiKey: 'BUrnf8yAQyCaW9au00hzKFrGUY3Pyuq9',
+    };
+
+    const geocoder = await NodeGeocoder(options);
+
+    try {
+      const  resultStr = await geocoder.reverse({
+        lat: 32.101786566878445,
+        lon: 34.858965073072056
+        // lat: asset.metadata.location.lat,
+        // lon: asset.metadata.location.lon
+      });
+
+      const result = JSON.parse(resultStr);
+
+      debug('got result:', result);      //----------------------------------------------------------------------------------------------------------------------------------------
+    }
+    catch(e) {
+      debug('failed execution:', e.stack)
+      //return this.error(500, {success: false, error: e.stack });
+    }
+  }
+
+  async run() {
+    await this.openMapquest();
+    await this.openStreetsMap();
+  }
 }
 
-debug('starting runner...');
-const runner = new GetLayersByBoundingBox();
-runner.handler();
+const runner = new Main().run();
